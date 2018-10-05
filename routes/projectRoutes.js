ca59e864c0983893743487da29c1129e1f528a06
Max <maxkajiwara@gmail.com>
2018-10-05 09:32:08 -0700

    Add GET: project by id, GET: actions by project id

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -23,4 +23,50 @@ router.get('/', (req, res) => {
 		});
 });
 
+// get project by id
+router.get('/:id', (req, res) => {
+	const id = req.params.id;
+	projectModel
+		.get(id)
+		.then(project => {
+			if (!project) {
+				console.log('\n=== NO PROJECT BY THAT ID ===\n');
+				return res
+					.status(404)
+					.json({ message: 'Project with specified ID does not exist.' });
+			}
+			console.log('\n=== RETURNED PROJECT: ===\n\n', project, '\n');
+			res.status(200).json(project);
+		})
+		.catch(err => {
+			console.log('\n=== SERVER ERROR ===\n\n', err);
+			res
+				.status(500)
+				.json({ error: 'Project information could not be retrieved.' });
+		});
+});
+
+// get actions by project id
+router.get('/:id/actions', (req, res) => {
+	const id = req.params.id;
+	projectModel
+		.getProjectActions(id)
+		.then(actions => {
+			if (!actions[0]) {
+				console.log('\n=== PROJECT HAS NO ACTIONS ===\n');
+				return res
+					.status(404)
+					.json({ message: 'Project with specified ID has no actions.' });
+			}
+			console.log('\n=== RETURNED PROJECT ACTIONS: ===\n\n', actions, '\n');
+			res.status(200).json(actions);
+		})
+		.catch(err => {
+			console.log('\n=== SERVER ERROR ===\n\n', err);
+			res
+				.status(500)
+				.json({ error: 'Project actions could not be retrieved.' });
+		});
+});
+
 module.exports = router;

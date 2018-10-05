2ab1ac8cb46c2a8000801d3ad15fa9c684dbdbf7
Max <maxkajiwara@gmail.com>
2018-10-05 09:56:22 -0700

    Add PUT: project by id

--- routes/projectRoutes.js
+++ routes/projectRoutes.js
@@ -69,4 +69,104 @@ router.get('/:id/actions', (req, res) => {
 		});
 });
 
+// add new project
+router.post('/', (req, res) => {
+	const { name, description } = req.body;
+	if (!name || !description) {
+		console.log(`\n=== PROJECT NAME AND/OR DESCRIPTION NOT PROVIDED ===\n\n`);
+		return res.status(400).json({
+			message: 'Please provide a name and description for the project.'
+		});
+	}
+
+	if (name.length > 128) {
+		console.log(`\n=== PROJECT NAME EXCEEDED 128 CHARACTER LIMIT ===\n\n`);
+		return res.status(400).json({
+			message: 'Project name cannot exceed 128 characters.'
+		});
+	}
+
+	projectModel
+		.insert({ name, description })
+		.then(project => {
+			if (!project) {
+				console.log('\n=== FAILED TO ADD PROJECT ===\n');
+				return res
+					.status(500)
+					.json({ error: 'Failed to add the project to the database.' });
+			}
+			console.log('\n=== ADDED PROJECT: ===\n\n', project, '\n');
+			res.status(201).json(project);
+		})
+		.catch(err => {
+			console.log('\n=== SERVER ERROR ===\n\n', err);
+			res.status(500).json({
+				error: 'There was an error while saving the project to the database'
+			});
+		});
+});
+
+// update project by id
+router.put('/:id', (req, res) => {
+	const { name, description } = req.body;
+	if (!name || !description) {
+		console.log(`\n=== PROJECT NAME AND/OR DESCRIPTION NOT PROVIDED ===\n\n`);
+		return res.status(400).json({
+			message: 'Please provide a name and description for the project.'
+		});
+	}
+
+	if (name.length > 128) {
+		console.log(`\n=== PROJECT NAME EXCEEDED 128 CHARACTER LIMIT ===\n\n`);
+		return res.status(400).json({
+			message: 'Project name cannot exceed 128 characters.'
+		});
+	}
+
+	projectModel
+		.update({ name, description })
+		.then(updatedProject => {
+			if (!updatedProject) {
+				console.log('\n=== NO PROJECT BY THAT ID ===\n\n');
+				return res
+					.status(404)
+					.json({ message: 'Project with the specified ID does not exist.' });
+			}
+			console.log(
+				`\n=== UPDATED PROJECT ===\n\n Project with id ${
+					req.params.id
+				} has been updated\n`
+			);
+			res.status(200).json({ message: 'Project was successfully updated.' });
+		})
+		.catch(err => {
+			console.log('\n=== SERVER ERROR ===\n\n', err);
+			res.status(500).json({ error: 'Project could not be modified.' });
+		});
+});
+
+// delete project by id
+router.delete('/:id', (req, res) => {
+	projectModel
+		.remove(req.params.id)
+		.then(removedProject => {
+			if (!removedProject) {
+				console.log(`\n=== NO PROJECT BY THAT ID ===\n\n`);
+				return res
+					.status(404)
+					.json({ message: 'Project with the specified ID does not exist.' });
+			}
+			console.log(
+				`\n=== DELETED PROJECT ===\n\n Project with id ${
+					req.params.id
+				} removed from db('projects')\n`
+			);
+			res.status(200).json({ message: 'Project was successfully deleted.' });
+		})
+		.catch(err => {
+			console.log('\n=== SERVER ERROR ===\n\n', err);
+			res.status(500).json({ error: 'Project could not be deleted' });
+		});
+});
+
 module.exports = router;
